1bbb979bb404c9260097bf5ec37c8f00066b6845
Grega Dvoršak <gdvorsak97@gmail.com>
2021-06-30 00:39:44 +0200

    Added utility function from the tensorflow implementation

---
+++ kinship_utils.py
@@ -0,0 +1,114 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from keras_vggface.utils import preprocess_input
+from tensorflow.python.keras.layers import Rescaling
+from tensorflow.python.keras.models import Sequential
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+
+def plot_loss(history, label, n):
+    # Use a log scale on y-axis to show the wide range of values.
+    plt.semilogy(history.epoch, history.history['loss'],
+                 color=colors[n], label='Train ' + label)
+    plt.semilogy(history.epoch, history.history['val_loss'],
+                 color=colors[n], label='Val ' + label,
+                 linestyle="--")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.show()
+
+
+def plot_metrics(history):
+    metrics = ['loss', 'auc', 'precision', 'recall']
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_", " ").capitalize()
+        plt.subplot(2, 2, n + 1)
+        plt.plot(history.epoch, history.history[metric], color=colors[0], label='Train')
+        plt.plot(history.epoch, history.history['val_' + metric],
+                 color=colors[0], linestyle="--", label='Val')
+        plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == 'auc':
+            plt.ylim([0, 1])  # originally from 0.8 to 1
+        else:
+            plt.ylim([0, 1])
+
+        plt.legend()
+    plt.show()
+
+
+def visualize_crop(in_img, crp_img):
+    in_img = cv2.cvtColor(in_img, cv2.COLOR_BGR2RGB)
+    crp_img = cv2.cvtColor(crp_img, cv2.COLOR_BGR2RGB)
+    fig, axes = plt.subplots(1, 2)
+    axes[0].imshow(in_img[:, :, :])
+    axes[0].set_title('Original image')
+    axes[1].imshow(crp_img[:, :, :])
+    axes[1].set_title('Cropped input')
+    fig.suptitle(f'Original and cropped input')
+    fig.set_size_inches(9, 5, forward=True)
+    plt.show()
+
+
+def alignment(image, path, visualize=False, save=False):
+
+    label_path = "D:\\Files on Desktop\\engine\\fax\\magistrska naloga\\Ankitas Ears\\bounding boxes alligment\\"
+    delete_ist_path = label_path + "delete list.txt"
+    filename = ""
+    if "/" in path:
+        family = path.split("/")[-3]
+        filename = path.split("/")[-1]
+        label_path += "labels " + str(family) + ".csv"
+    elif "\\" in path:
+        filename = path.split("\\")[-1]
+        label_path = label_path + "all_labels.csv"
+    label_file = pd.read_csv(label_path)
+    bbox_data = label_file[label_file['file'] == filename]
+    bbox = image[bbox_data['y1'].values[0]:bbox_data['y1'].values[0] + bbox_data['dy'].values[0],
+           bbox_data['x1'].values[0]:bbox_data['x1'].values[0] + bbox_data['dx'].values[0]]
+    if visualize:
+        cv2.imshow("Detected", bbox)
+        cv2.waitKey()
+    if save:
+        cv2.imwrite("example.png", bbox)
+    return bbox
+
+
+def crop_ears(img, region):
+    if region == "left":
+        img = img[:, 0:int(np.round(224 / 3))]
+    elif region == "right":
+        img = img[:, -int(np.round(224 / 3)):]
+    elif region == "mid_vertical":
+        img = img[:, int(np.round(224 / 6)):int(np.round(224 / 6)) + int(np.round(224 / 3))]
+    elif region == "top":
+        img = img[0:int(np.round(224 / 3)), :]
+    elif region == "mid_horizontal":
+        img = img[int(np.round(224 / 6)):int(np.round(224 / 6)) + int(np.round(224 / 3)), :]
+    elif region == "bottom":
+        img = img[-int(np.round(224 / 3)):, :]
+    return img
+
+
+rescale = Sequential([
+    Rescaling(1. / 255)
+])
+
+
+# read images
+def read_img(path):
+    in_img = cv2.imread(path)
+    in_img = alignment(in_img, path)
+    in_img = cv2.resize(in_img, (224, 224))
+    img = crop_ears(in_img, "top")
+    img = cv2.resize(img, (224, 224))
+    # visualize_crop(in_img, img)
+    img = np.array(img, dtype="float64")
+    img = preprocess_input(img, version=2)  # 1 for VGG, 2 otherwise
+    img = rescale(img)  # resize and rescale are Resize and Normalize in torch
+    return img
