af7a627b6e886435fdf380680ae7f26b6ce875f5
Grega Dvoršak <gdvorsak97@gmail.com>
2021-07-12 19:43:10 +0200

    Training now running

--- baseline_aff.py
+++ baseline_aff.py
@@ -11,18 +11,23 @@ from kinship_utils import free_gpu_cache
 # Find a tutorial on how to train a net on imagenet in pytorch or just copy the basic example and use this loader
 # torchvision.datasets.ImageNet(root, split='train')
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                         std=[0.5, 0.5, 0.5])
+])
 
 batch_size = 4
-path = "D:\\Files on Desktop\\engine\\fax\\magistrska naloga\\Imagenet\\Data\\CLS-LOC"
+path = "D:\\Files on Desktop\\engine\\fax\\magistrska naloga\\Imagenet\\subset\\data\\"
+train_path = path + "train"
+test_path = path + "test"
 
-train_set = torchvision.datasets.ImageNet(root=path, train=True, transform=transform)
-train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+train_set = torchvision.datasets.ImageFolder(root=train_path, transform=transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
-test_set = torchvision.datasets.ImageNet(root=path, train=False, transform=transform)
-test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
+test_set = torchvision.datasets.ImageFolder(root=test_path, transform=transform)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 
 free_gpu_cache()
@@ -33,12 +38,13 @@ net = resnet152(fuse_type='DAF', small_input=False).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-NUM_EPOCHS = 40
+NUM_EPOCHS = 10
 
-for epoch in range(NUM_EPOCHS):  # loop over the dataset multiple times
 
+def train():
+    net.train()
     running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
+    for i, data in enumerate(train_loader):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data[0].to(device), data[1].to(device)
 
@@ -54,9 +60,13 @@ for epoch in range(NUM_EPOCHS):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+            print('[%5d] loss: %.3f' %
+                  ( i + 1, running_loss / 2000))
             running_loss = 0.0
+
+
+for epoch in range(NUM_EPOCHS):  # loop over the dataset multiple times
+    train()
 
 print('Finished Training')
 
